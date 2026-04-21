37f490e068b4b496c0d6fecbaeaf0747ad743d09
Thomas Lynn <132193278+thomaslynn132@users.noreply.github.com>
2026-04-21 14:59:22 +0630

    Enable CORS middleware in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import cors from 'cors';
+import cors from 'cors';
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes/index.js";
@@ -9,7 +9,7 @@ import config from "./config/index.js";
 const app = express();
 
 app.use(helmet());
-// app.use(cors({ origin: config.clientUrl, credentials: true }));
+app.use(cors({ origin: config.clientUrl, credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
